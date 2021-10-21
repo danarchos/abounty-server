63457627533f89dc7484122431ae7116c7052ed5
Daniel McGrane <danielmcgrane@Daniels-Mac-mini.local>
2021-10-21 16:49:56 +0100

    removed comments

--- app.ts
+++ app.ts
@@ -45,36 +45,3 @@ app.listen(PORT, async () => {
   const allNodes = await db.getAllSupaNodes();
   await ln.reconnectNode(allNodes);
 });
-
-//
-// // Bounties
-// //
-// app.get("/live-bounties", catchAsyncErrors(bountyRoutes.liveBounties));
-// app.get("/bounty/:id", catchAsyncErrors(bountyRoutes.bounty));
-// app.get("/rewards/:username", catchAsyncErrors(bountyRoutes.getRewards));
-// app.get("/reward/:id", catchAsyncErrors(bountyRoutes.getReward));
-// app.post("/create-bounty", catchAsyncErrors(bountyRoutes.createBounty));
-// app.post("/update-speaker", catchAsyncErrors(bountyRoutes.updateSpeaker));
-// app.post("/complete-bounty", catchAsyncErrors(bountyRoutes.completeBounty));
-// app.post("/expire-bounty", catchAsyncErrors(bountyRoutes.expireBounty));
-
-// //
-// // LN Routes
-// //
-// app.post("/connect", catchAsyncErrors(lnRoutes.connect));
-
-// app.post(
-//   "/create-bounty-invoice",
-//   catchAsyncErrors(lnRoutes.createBountyInvoice)
-// );
-// app.post("/create-invoice", catchAsyncErrors(lnRoutes.createInvoice));
-// app.post("/cancel-invoice", catchAsyncErrors(lnRoutes.cancelInvoice));
-// app.post("/settle-invoice", catchAsyncErrors(lnRoutes.settleInvoice));
-// app.get("/get-invoice", catchAsyncErrors(lnRoutes.getInvoice));
-
-// app.get("/withdraw-request", catchAsyncErrors(lnRoutes.withdrawRequest));
-// app.get("/initiate-withdrawal", catchAsyncErrors(lnRoutes.initiateWithdrawal));
-// app.get("/execute-withdrawal", catchAsyncErrors(lnRoutes.executeWithdrawal));
-
-// // Twitter routes
-// app.get("/usernames", catchAsyncErrors(twitterRoutes.usernames));
